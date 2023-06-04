97475a743fbf3a4392eb3677e217f60ca216d0d3
Matan <matanmazor@outlook.com>
2023-06-04 21:06:20 +0100

    break line as a class

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,9 +1,10 @@
-
 $(".header").click(function () {
 
     $header = $(this);
     //getting the next element
     $content = $header.next();
+    //find the break line that corresponds to this header
+    $breakLine = $header.parent().siblings('.break-line');
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
@@ -12,7 +13,7 @@ $(".header").click(function () {
             //change text based on condition
             return $content.is(":visible") ? "Collapse" : "Expand";
         });
+        //toggle the visibility of the break line
+        $breakLine.toggle();
     });
-    //toggle the visibility of the break line
-    $("#break-line").toggle();
 });
